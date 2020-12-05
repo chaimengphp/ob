4452f9ebd1799d84e2777fcdd52a44e95272312f
柴萌 <chaimengmeng@jiemian.com>
2020-12-05 16:08:33 +0800

    接口开发1

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `ob_user` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
-  `username` varchar(30) NOT NULL COMMENT '登录账户',
+  `device_id` varchar(30) NOT NULL COMMENT '登录设备',
   `password` varchar(30) NOT NULL COMMENT '登录密码',
   `nike_name` varchar(50) NOT NULL COMMENT '昵称',
   `reg_from` varchar(20) NOT NULL COMMENT '注册来源',
@@ -9,7 +9,7 @@ CREATE TABLE `ob_user` (
   `head_img` varchar(150) NOT NULL COMMENT '头像地址',
   `summary` varchar(250) DEFAULT NULL COMMENT '个性签名',
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM AUTO_INCREMENT=100002 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=100000 DEFAULT CHARSET=utf8;
 
 
 CREATE TABLE `ob_user_content` (
@@ -20,3 +20,15 @@ CREATE TABLE `ob_user_content` (
   `pubtime` int(10) unsigned NOT NULL COMMENT '发布时间',
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+
+CREATE TABLE `ob_pet` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT COMMENT '主键',
+  `user_id` int(10) unsigned NOT NULL COMMENT '用户uid',
+  `head_img` varchar(150) NOT NULL COMMENT '头像地址',
+  `pet_name` varchar(50) NOT NULL COMMENT '名字',
+  `pet_type` varchar(50) NOT NULL COMMENT '品种',
+  `pet_age` tinyint(2) unsigned NOT NULL COMMENT '年龄',
+  `pet_sex` varchar(50) NOT NULL COMMENT '性别',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
